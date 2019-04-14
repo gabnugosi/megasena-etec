d381e70b2e8d46eb655de44593a0ac3a951b0f55
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-04-14 00:23:44 -0300

    Criação da API

--- src/main/java/br/com/etechoracio/megasena/MegasenaApplication.java
+++ src/main/java/br/com/etechoracio/megasena/MegasenaApplication.java
@@ -9,5 +9,6 @@ public class MegasenaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MegasenaApplication.class, args);
 	}
-
+	//Matar um projeto na porta 8080
+//https://superuser.com/questions/810659/kill-a-process-which-is-listening-on-port-8080
 }
